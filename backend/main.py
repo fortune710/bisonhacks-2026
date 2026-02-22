6d49759a4884f10faeedd416f42f3e70a4c9acb6
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2026-02-21 20:11:14 -0500

    added port to config

--- backend/main.py
+++ backend/main.py
@@ -21,4 +21,4 @@ def check_eligibility():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.PORT, reload=True)
